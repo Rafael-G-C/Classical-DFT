5ff83f35c79b02543ab27eb288179e869a0bfc9e
Rafael <rafael.g.p.p@outlook.com>
2020-09-13 10:55:37 -0500

    fixed some issues

--- c-dft.py
+++ c-dft.py
@@ -11,6 +11,15 @@ def lenard_jones(d):
     lj_potential = 4 * 1.6 * ( ( (2.6/d)**12 ) - ((2.6/d)**6) )
     return lj_potential
 
+# dummy_correlation
+def dummy_correlation(prev_density,calc_density):
+    diff_density = calc_density - prev_density
+    if diff_density < 0:
+        new_density =  (calc_density + (diff_density)) * -0.05
+    else:
+        new_density =  (calc_density + (diff_density)) * 0.05
+    return new_density
+
 #Variables
 distances = np.linspace(1,10,100)# angstroms, starting distance, ending distance, points between the start and the end
 densities = np.linspace(0.01,0.1,100)# molecules/angstrom^3 starting density, ending density, points between the start and the end
@@ -31,15 +40,19 @@ chem_potential = np.log(delta**3 * bulk_density)/beta #
 #is used in the analytical solution
 a_distance_v_density = {}
 #is used in the numerical aproximation
-helmoltz_v_density = {} #will hold the total energy per density as key and the calculated density as value
+
 distance_v_density = {} # will hold all the disctances as keys and the densities (the ones that lowered the total energy)
+prev_density = 0 
 for i in range(len(distances)-1):
+    helmoltz_v_density = {} #will hold the total energy per density as key and the calculated density as value
     distance = distances[i] # get a distance from the list of distances
     ext_p = lenard_jones(distance) # define an external potential here
     
-    #analitical solution
-    a_distance_v_density[distance] = bulk_density * np.exp(-beta*ext_p)
-
+    #analytical solution
+    calc_density = bulk_density * np.exp(-beta*ext_p)
+    correlation = dummy_correlation(prev_density,calc_density)
+    a_distance_v_density[distance] = calc_density + correlation
+    prev_density = calc_density + correlation
     #Numerical aproximation
     # calculate the total energy for every density in the list
     for density in densities:
@@ -49,7 +62,6 @@ for i in range(len(distances)-1):
     #search for the minimum total energy and added as a value of the given distance
     distance_v_density[distance] = helmoltz_v_density[min(helmoltz_v_density)]
     print(f"{distance},{distance_v_density[distance]},{a_distance_v_density[distance]}") #simple output
-    helmoltz_v_density = {} # clean the dictionary so that old data doesn't interfere
 
 
 
