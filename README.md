251c0b8f6ef2ad090420139d26317a74916230ab
Rafael <rafael.g.p.p@outlook.com>
2020-12-05 22:11:49 -0600

    removed dummy-correlation and added README.md

---
+++ README.md
@@ -0,0 +1,22 @@
+#A python code based on the article by Jeanmairet et al.(https://doi.org/10.1021/ed500049m)
+
+on lines
+
+```python
+distances = np.linspace(1,10,100)# angstroms, starting distance, ending distance, points between the start and the end
+densities = np.linspace(0.01,0.1,100)# molecules/angstrom^3 starting density, ending density, points between the start and the end
+```
+In _distances_ we can change the maximum distance and how many points we want to calculate while in _densities_ we can change how many densities should the numerical aproximation try for the minimization
+
+to keep things as simple as possible the code outputs the data
+
+```
+distances,numerical_density,analytical density
+1.0,0.01,0.0
+1.0909090909090908,0.01,0.0
+1.1818181818181819,0.01,0.0
+1.2727272727272727,0.01,0.0
+1.3636363636363638,0.01,0.0
+1.4545454545454546,0.01,0.0
+1.5454545454545454,0.01,0.0
+```
--- c-dft.py
+++ c-dft.py
@@ -4,6 +4,7 @@ import numpy as np
 def ideal_p(distance, density):
     i_fluid = 1/beta * distance * density * (np.log((delta**3)*density) - 1)
     return i_fluid
+
 #Lenard-Jones potential
 def lenard_jones(d):
     #Epsilon = 1.6 KJ/mol
@@ -11,25 +12,16 @@ def lenard_jones(d):
     lj_potential = 4 * 1.6 * ( ( (2.6/d)**12 ) - ((2.6/d)**6) )
     return lj_potential
 
-# dummy_correlation
-def dummy_correlation(prev_density,calc_density):
-    diff_density = calc_density - prev_density
-    if diff_density < 0:
-        new_density =  (calc_density + (diff_density)) * -0.05
-    else:
-        new_density =  (calc_density + (diff_density)) * 0.05
-    return new_density
-
 #Variables
 distances = np.linspace(1,10,100)# angstroms, starting distance, ending distance, points between the start and the end
 densities = np.linspace(0.01,0.1,100)# molecules/angstrom^3 starting density, ending density, points between the start and the end
 
 # Constants
-kb = 1.38065E-26 # Boltzmann constant, KJ/K
-T = 300 # Temparute, K 
+kb = 1.38065E-26 # Boltzmann constant, kJ/K
+T = 300 # Temperature, K 
 Na = 6.022E23 # Avogrado's constant, 1/mol
 atom_mass = 20 # mass, u
-plank_constant = 6.26E-37 # KJ * s
+plank_constant = 6.26E-37 # kJ * s
 bulk_density = 0.033 # molecules / angstrom^3 
 
 #Calculated constants
@@ -43,6 +35,7 @@ a_distance_v_density = {}
 
 distance_v_density = {} # will hold all the disctances as keys and the densities (the ones that lowered the total energy)
 prev_density = 0 
+print("distances,numerical_density,analytical density")
 for i in range(len(distances)-1):
     helmoltz_v_density = {} #will hold the total energy per density as key and the calculated density as value
     distance = distances[i] # get a distance from the list of distances
@@ -50,16 +43,15 @@ for i in range(len(distances)-1):
     
     #analytical solution
     calc_density = bulk_density * np.exp(-beta*ext_p)
-    correlation = dummy_correlation(prev_density,calc_density)
-    a_distance_v_density[distance] = calc_density + correlation
-    prev_density = calc_density + correlation
+    a_distance_v_density[distance] = calc_density
+    prev_density = calc_density
     #Numerical aproximation
     # calculate the total energy for every density in the list
     for density in densities:
          
         e_tot = ideal_p(distance,density) + (distance*ext_p*density) - distance*chem_potential*density
         helmoltz_v_density[e_tot] = density
-    #search for the minimum total energy and added as a value of the given distance
+    #search for the minimum total energy and add it as a value for the given distance
     distance_v_density[distance] = helmoltz_v_density[min(helmoltz_v_density)]
     print(f"{distance},{distance_v_density[distance]},{a_distance_v_density[distance]}") #simple output
 
